772f04ed3c537e12b92a66b8514e9aa7488bcae5
dasankita-hub <ankitdas3002@gmail.com>
2024-10-09 01:37:36 +0530

    commit

---
+++ FrontEnd/Javascript/class2/datatype.js
@@ -0,0 +1,60 @@
+
+let data;
+console.log(typeof data);
+console.log(data);
+
+//number
+let age=22;
+console.log("My age is : ",age);
+console.log(typeof age);
+let pi=3.14;
+console.log(pi);
+console.log(typeof pi);
+//let num=08; error because decimal with leading zero are not allowed here
+
+//Addition
+let num1=11;
+let num2=4;
+let addition=num1+num2;
+console.log(addition)
+//sub
+num1=11;
+ num2=4;
+let sub=num1-num2;
+console.log(sub);
+
+ num1=11;
+num2=4;
+ let mul=num1*num2;
+console.log(mul);
+num1=11;
+num2=4;
+let div=num1/num2;
+console.log(div);
+
+// boolean
+     
+
+
+
+//string
+// let ch='Ankita';
+// let name="kaju";
+// console.log(ch,name)
+
+let name=" Ankita ";
+let ag =21;
+let stream="MCA";
+let roll=11;
+let city="Kendrapada";
+let clgName="USBM";
+console.log('My name is ${name} ,my age is : ${age} , stream is ${stream},city name is :${city},college name is :${clgName} ,roll no is: ${roll}');
+console.log(" The length of the city is : ",city.length);
+
+let my_website="www.ankitacode.com";
+let b=3;
+let c="bc"
+console.log(b+c);
+console.log(b-c);//NaN
+console.log(b*c);//NaN
+console.log(b/c);//NaN
